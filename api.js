fb72e02f5a5842cdac84f795f86ce04f9cfd053f
Benjamin Girvan <bengirvan@fb.com>
2022-10-26 12:57:05 +0100

    Added error message for empty query and api.js file

---
+++ api.js
@@ -0,0 +1,7 @@
+const fetch = require("isomorphic-fetch")
+const key = "12517e9852ae2916fbb7e9018fd85977"
+const url = "https://api.themoviedb.org/3/"
+
+async function getMovieData(nameToQuery) {
+
+}
--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const validate = () => {
     let movieToQuery = document.getElementById("id").value
     console.log(movieToQuery)
-
+    let p = document.createElement("p")
+    if (movieToQuery === "") {
+        let text = document.createTextNode("No movie entered")
+        p.appendChild(text)
+        let element = document.getElementById("d1")
+        element.appendChild(p)
+    }
 }
 validate()
 let button = document.getElementById("button")
