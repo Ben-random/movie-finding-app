56909fb66f9bc63c1a44eef091c812355151264a
Benjamin Girvan <bengirvan@fb.com>
2022-10-26 10:55:36 +0100

    Added project fies

---
+++ script.js
@@ -0,0 +1,9 @@
+function main(){
+    const validate = () => {
+        let movieToQuery = document.forms["movie_query"]["movie_to_query"]
+        console.log(movieToQuery)
+    }
+    validate()
+}
+
+module.exports = main
